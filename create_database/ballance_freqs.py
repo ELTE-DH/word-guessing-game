5c1a578badf178be005c36b0a20aa47f48405bb1
Balázs Indig <dlazesz@users.noreply.github.com>
2022-01-04 20:07:33 +0100

    Reproducible sampling

--- create_database/ballance_freqs.py
+++ create_database/ballance_freqs.py
@@ -11,29 +11,39 @@ seed(12345)
 
 
 def select_elems(inp_fh, out_fh, no_of_elements=30):
-    prev_word = None
     conc_list = []
+    lefts = set()
+    rights = set()
+    sents = set()
+    words = set()
+    left_size, right_size = 4, 4
     for line in inp_fh:
         line = line.rstrip()
         word, left, right, sent = line.split('\t', maxsplit=3)
-        if word == prev_word:
-            conc_list.append(line)
-        else:
-            sample_elems(conc_list, no_of_elements, out_fh)
-            prev_word = word
-            conc_list.append(line)
-    sample_elems(conc_list, no_of_elements, out_fh)
+        # TODO generalize this!
+        left_truncated, right_truncated = turncate_context(left, right, left_size, right_size)
+        # Checking the smallest context is enough!
+        if word not in words and sent not in sents and left_truncated not in lefts and right_truncated not in rights:
+            lefts.add(left_truncated)
+            rights.add(right_truncated)
+            sents.add(sent)
+            words.add(word)
+            conc_list.append((word, left, right, sent))
+
+    chosen_concs = sample(conc_list, no_of_elements)
+    for sent_id, (word, left, right, sent) in enumerate(chosen_concs, start=1):
+        # TODO generalize this
+        for left_size, right_size in ((4, 0), (0, 4), (4, 4), (8, 0), (0, 8), (8, 8)):
+            left_truncated, right_truncated = turncate_context(left, right, left_size, right_size)
+            print(word, left_truncated, right_truncated, sent, sent_id, sep='\t', file=out_fh)
 
 
-def sample_elems(conc_list, no_of_elements, out_fh):
-    freq = len(conc_list)
-    if freq > no_of_elements:
-        chosen_concs = sample(conc_list, no_of_elements)
-    else:
-        chosen_concs = conc_list
-    for c in chosen_concs:
-        print(c, freq, sep='\t', file=out_fh)
-    conc_list.clear()
+def turncate_context(left, right, left_size, right_size):
+    left_split = left.split(' ')
+    right_split = right.split(' ')
+    left_truncated = ' '.join(left_split[max(len(left_split) - left_size, 0):])
+    right_truncated = ' '.join(right_split[:min(right_size, len(right_split))])
+    return left_truncated, right_truncated
 
 
 if __name__ == '__main__':
