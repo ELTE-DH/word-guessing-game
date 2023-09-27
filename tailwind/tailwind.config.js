d3aa7923d85375718e4051860d85a4036f37eb24
Gortkab <82842216+Gortkab@users.noreply.github.com>
2023-09-27 23:20:04 +0200

    classes created, cleaner layout.html

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -7,22 +7,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        dhgreen: '#175F37',
-        dhgreen2: '#134E2D',
         dhbronze1: '#998675',
         dhbronze2: '#75675a',
-      },
-      animation: {
-        slide: "slide 2.5s linear infinite",
-      },
-      keyframes: {
-        slide: {
-          "0%": { transform: "translateY(100%)", opacity: 0.1 },
-          "15%": { transform: "translateY(0)", opacity: 1 },
-          "30%": { transform: "translateY(0)", opacity: 1 },
-          "45%": { transform: "translateY(-100%)", opacity: 1 },
-          "100%": { transform: "translateY(-100%)", opacity: 0.1 },
-        },
       },
     },
   },
@@ -31,12 +17,7 @@ module.exports = {
   },
   plugins: [],
   safelist: [
-    'bg-red-500',
-    'bg-green-500',
-    'bg-blue-500',
-    'dh-btn-green1',
+    'dh-btn-bronze1',
     'dh-btn-disabled',
-    'translate-y-20',
-    'opacity-0',
   ]
 }
